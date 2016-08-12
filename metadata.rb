08969c8a129bad4c22cbbe2a11723ede17a6cb6a
Christopher Chow <christopher.chow@kinesis.org>
2016-08-12 12:20:08 +1000

    Bump to v1.0.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Christopher Chow"
 maintainer_email "chris@chowie.net"
 license          "MIT"
 description      "Installs and configures nginx"
-version          "1.0.0"
+version          "1.0.1"
 
 depends "apt"
 
