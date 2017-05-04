41c9032157337d68caf235c8538ccca7c6dfd412
James Crisp <james.crisp@kinesis.org>
2017-05-04 17:52:55 +1000

    chore(version): bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Christopher Chow"
 maintainer_email "chris@chowie.net"
 license          "MIT"
 description      "Installs and configures nginx"
-version          "1.0.1"
+version          "1.0.2"
 
 depends "apt"
 
